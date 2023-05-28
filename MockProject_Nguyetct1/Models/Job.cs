8c1f288fa33a9e31242ae3d7cf17b5f63edfb932
sangnt03 <sangnt03@acb.com.vn>
2023-05-28 11:02:08 +0700

    change name

--- MockProject_Nguyetct1/Models/Job.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MockProject_Nguyetct1.Models
-{
-    public class Job
-    {
-        public int Id { get; set; }
-        public string? JobTitle { get; set; }
-        public string? JobDescription { get; set; }
-    }
-}
